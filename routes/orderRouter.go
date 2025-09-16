19b9652ffbdcb9c14ffba32d956183d060c43a56
rahel yabebal <rahelyabebal@gmail.com>
2025-09-16 12:45:28 +0300

    defined table and order routes

--- routes/orderRouter.go
+++ routes/orderRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/rahel-yab/Restuarant-Management/controllers"
+
+)
+
+func OrderRouters(incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/orders" , controllers.GetOrders())
+	incomingRoutes.GET("/orders/:order_id" , controllers.GetOrder())
+	incomingRoutes.POST("/orders" , controllers.CreateOrder())
+	incomingRoutes.PATCH("/orders/:order_id" , controllers.UpdateOrder())
+
+}
--- routes/tableRouter.go
+++ routes/tableRouter.go
@@ -1,1 +1,16 @@
 package routes
+
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/rahel-yab/Restuarant-Management/controllers"
+
+)
+
+func TableRouters(incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/tables" , controllers.GetTables())
+	incomingRoutes.GET("/tables/:table_id" , controllers.GetTable())
+	incomingRoutes.POST("/tables" , controllers.CreateTable())
+	incomingRoutes.PATCH("/tables/:Table_id" , controllers.UpdateTable())
+
+}
