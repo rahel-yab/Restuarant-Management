ab628c63d2a77577408343d376b2ca2272e00df0
rahel yabebal <rahelyabebal@gmail.com>
2025-09-16 12:31:24 +0300

    defined food and user routes

--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/rahel-yab/Restuarant-Management/controllers"
+
+)
+
+func FoodRoutes(incominRoutes *gin.Engine){
+	incominRoutes.GET("/foods" , controllers.GetFoods())
+	incominRoutes.GET("/foods/:food_id" , controllers.GetFood())
+	incominRoutes.POST("/foods" , controllers.CreateFood())
+	incominRoutes.PATCH("/foods/:food_id" , controllers.UpdateFood())
+
+}
