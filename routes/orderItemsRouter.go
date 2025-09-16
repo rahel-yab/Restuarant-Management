51755a454cd91837540a262fd6d3654e3cc779be
rahel yabebal <rahelyabebal@gmail.com>
2025-09-16 12:51:59 +0300

    defined orderItem routes

--- routes/orderItemsRouter.go
+++ routes/orderItemsRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/rahel-yab/Restuarant-Management/controllers"
+
+)
+
+func OrderItemRouters(incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/orderItems" , controllers.GetOrderItems())
+	incomingRoutes.GET("/orderItems/:orderItem_id" , controllers.GetOrderItem())
+	incomingRoutes.POST("/orderItems" , controllers.CreateOrderItem())
+	incomingRoutes.PATCH("/orderItems/:orderItem_id" , controllers.UpdateOrderItem())
+	incomingRoutes.GET("/orderItmes-order/:order_id", controllers.GETOrderItemsByOrder())
+}
