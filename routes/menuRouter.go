00e263f46d01a3300e55306f8c33c43e76c9bbf2
rahel yabebal <rahelyabebal@gmail.com>
2025-09-16 12:42:11 +0300

    defined menuRoutes

--- routes/menuRouter.go
+++ routes/menuRouter.go
@@ -1,1 +1,15 @@
 package routes
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/rahel-yab/Restuarant-Management/controllers"
+
+)
+
+func MenuRouters(incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/menus" , controllers.GetMenus())
+	incomingRoutes.GET("/menus/:menu_id" , controllers.GetMenu())
+	incomingRoutes.POST("/menus" , controllers.CreateMenu())
+	incomingRoutes.PATCH("/menus/:menu_id" , controllers.UpdateMenu())
+
+}
