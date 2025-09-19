26d755b89a8f730fee803db96bf37399bf0bd460
rahel yabebal <rahelyabebal@gmail.com>
2025-09-19 23:58:26 +0300

    finalized db connection

--- database/dbConnection.go
+++ database/dbConnection.go
@@ -30,3 +30,10 @@ func DBinstance() *mongo.Client{
 	fmt.Println("Connected to mongoDB")
 	return client
 }
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) * mongo.Collection{
+	var collection *mongo.Collection = client.Database("RestaurantDB").Collection(collectionName)
+	return collection
+}
