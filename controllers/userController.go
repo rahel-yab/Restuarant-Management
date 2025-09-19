77f6fb7afdfb5a6a3810fa6478f29d50f72ff225
rahel yabebal <rahelyabebal@gmail.com>
2025-09-20 00:06:35 +0300

    defined the structure for user controller funcs

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,38 @@
 package controllers
+
+import(
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context){
+		
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context){
+		
+	}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context){
+		
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context){
+		
+	}
+}
+
+func HashPassword(password string) string{
+
+	return
+}
+
+func VerifyPassword(password string , providePassword string) bool{
+	return 
+} 
