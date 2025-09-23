2a8f3d814a2ec68dc59d27dee50324158ef02b59
rahel yabebal <rahelyabebal@gmail.com>
2025-09-23 21:58:22 +0300

    updated food_id to type string

--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -1,1 +1,17 @@
 package models
+
+import(
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Invoice struct{
+	ID					primitive.ObjectID  		`bson:"_id"`
+	Invoice_id
+	Order_id
+	Payment_method
+	Payment_status
+	Payment_due_date
+	Created_at
+	updated_at
+}
