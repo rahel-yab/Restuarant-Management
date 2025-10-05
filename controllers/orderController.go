b194e5b8a77db4d8bb49448f7cf835f036883d47
rahel yabebal <rahelyabebal@gmail.com>
2025-10-05 12:22:19 +0300

    implemented get orders

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,13 +1,37 @@
 package controllers
 
-import(
+import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rahel-yab/Restuarant-Management/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		
+		result , err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error" : "error occured while listing order items"})
+
+		}
+		var allOrders []bson.M
+		if err = result.All(ctx, &allOrders); err != nil{
+			
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing food items"})
+			return
+		}
+		c.JSON(http.StatusOK, allOrders)
 	}
+
 }
 
 func GetOrder() gin.HandlerFunc {
