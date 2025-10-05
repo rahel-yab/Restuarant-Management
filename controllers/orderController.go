785e28cac11f12f03732322bac422fe3fa211b81
rahel yabebal <rahelyabebal@gmail.com>
2025-10-05 12:24:46 +0300

    implemented get order

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/rahel-yab/Restuarant-Management/database"
+	"github.com/rahel-yab/Restuarant-Management/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -36,7 +37,16 @@ func GetOrders() gin.HandlerFunc {
 
 func GetOrder() gin.HandlerFunc {
 	return func(c *gin.Context){
-		
+		var ctx , cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		orderId := c.Param("order_id")
+		var order models.Order
+
+		err := orderCollection.FindOne(ctx, bson.M{"order_id":orderId}).Decode(&order)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while fetching the order item"})
+		}
+		c.JSON(http.StatusOK, order)
 	}
 }
 
