5e2d8d5ba1de70836836e82656d61d7161acd557
rahel yabebal <rahelyabebal@gmail.com>
2025-10-22 23:24:31 +0300

    finalized table controller

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -3,7 +3,6 @@ package controllers
 
 import(
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -15,6 +14,7 @@ import(
 	"github.com/rahel-yab/Restuarant-Management/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
